19da52c5725a3c6ddc5939935690f88b92bafefd
FlacoJones <andrew@openq.dev>
2022-06-03 16:36:33 -0500

    capitalizes Authorization

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ module.exports = async function Probot(app, { getRouter }) {
 	router.use(bodyParser.urlencoded({ extended: true }));
 	router.use(express.json());
 	router.use((req, res, next) => {
-		if (!req.headers.authorization) {
+		if (!req.headers.Authorization) {
 			return res.status(403).json({ error: 'No credentials sent!' });
 		} else {
-			if (req.headers.authorization == process.env.GITHUB_BOT_SECRET) {
+			console.log('process.env.GITHUB_BOT_SECRET', process.env.GITHUB_BOT_SECRET);
+			console.log(req.headers);
+			if (req.headers.Authorization == process.env.GITHUB_BOT_SECRET) {
 				next();
 			} else {
 				return res.status(401).json({ error: 'Invalid Credentials' });
