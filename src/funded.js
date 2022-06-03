11514a425d7466031de097890a420db2884750bb
FlacoJones <andrew@openq.dev>
2022-06-03 15:46:24 -0500

    adds API secret

--- src/funded.js
+++ src/funded.js
@@ -5,25 +5,25 @@ const selectTokenMetadata = require('./utils');
 // Funded expects bountyId, id, and the deposit Obj of the deposit being funded
 // Makes a comment by Pat owner on issue with the bounty Id with a link to the bounty and the amount funded.
 async function funded(appOctokit, router) {
-    router.post('/funded', async (req, res) => {
-        const { deposit } = req.body;
-        const tokenMetadata = selectTokenMetadata();
-        const token = tokenMetadata[deposit.tokenAddress];
-        const { decimals } = tokenMetadata[deposit.tokenAddress];
+	router.post('/funded', async (req, res) => {
+		const { deposit } = req.body;
+		const tokenMetadata = selectTokenMetadata();
+		const token = tokenMetadata[deposit.tokenAddress];
+		const { decimals } = tokenMetadata[deposit.tokenAddress];
 
-        const formattedVolume = ethers.utils.formatUnits(deposit.tokenVolumes.toString(), decimals);
-        const { name } = token;
-        try {
-            const mutation = await appOctokit.graphql(ADD_COMMENT, {
-                id: req.body.bountyId,
-                body: `A deposit of  ${formattedVolume} ${name} was placed on this issue at ${process.env.BASE_URL}/bounty/${req.body.id}`,
-            });
-            res.json(mutation);
-        } catch (err) {
-            console.err(err);
-        }
-        res.status(200).end();
-    });
+		const formattedVolume = ethers.utils.formatUnits(deposit.tokenVolumes.toString(), decimals);
+		const { name } = token;
+		try {
+			const mutation = await appOctokit.graphql(ADD_COMMENT, {
+				id: req.body.bountyId,
+				body: `A deposit of  ${formattedVolume} ${name} was placed on this issue at ${process.env.BASE_URL}/bounty/${req.body.id}`,
+			});
+			res.json(mutation);
+		} catch (err) {
+			console.error(err);
+		}
+		res.status(200).end();
+	});
 }
 
 module.exports = funded;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ module.exports = async function Probot(app, { getRouter }) {
 	const router = getRouter('/');
 	router.use(bodyParser.urlencoded({ extended: true }));
 	router.use(express.json());
+	router.use((req, res, next) => {
+		if (!req.headers.authorization) {
+			return res.status(403).json({ error: 'No credentials sent!' });
+		} else {
+			if (req.headers.authorization == 'abc123') {
+				next();
+			} else {
+				return res.status(401).json({ error: 'Invalid Credentials' });
+			}
+		}
+	});
 	created(appOctokit, router, user);
 	funded(appOctokit, router, user);
 	refunded(appOctokit, router, user);
