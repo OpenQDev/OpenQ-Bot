07cf8ce6267b47535ba9725756af154a3d4c94e7
ArcAnya <arcanyatwitch@gmail.com>
2022-10-14 17:38:12 +0200

    change /bounty/ to /contract/ in links & denomation in strings

--- src/events/created.js
+++ src/events/created.js
@@ -1,18 +1,18 @@
-const { ADD_COMMENT } = require('../query');
+const { ADD_COMMENT } = require("../query");
 
 // Created expects bountyId and id. Makes a comment by Pat owner on issue with the bounty Id with a link to the bounty.
 async function created(authenticatedGraphQl, app) {
-	app.post('/created', async (req, res) => {
-		try {
-			const mutation = await authenticatedGraphQl(ADD_COMMENT, {
-				id: req.body.bountyId,
-				body: `A bounty has been minted for this issue at ${process.env.BASE_URL}/bounty/${req.body.bountyId}/${req.body.id}`,
-			});
-			res.json(mutation);
-		} catch (err) {
-			console.error(err);
-		}
-		res.status(200).end();
-	});
+  app.post("/created", async (req, res) => {
+    try {
+      const mutation = await authenticatedGraphQl(ADD_COMMENT, {
+        id: req.body.bountyId,
+        body: `A contract has been minted for this issue at ${process.env.BASE_URL}/contract/${req.body.bountyId}/${req.body.id}`,
+      });
+      res.json(mutation);
+    } catch (err) {
+      console.error(err);
+    }
+    res.status(200).end();
+  });
 }
 module.exports = created;
--- src/events/funded.js
+++ src/events/funded.js
@@ -17,7 +17,7 @@ async function funded(authenticatedGraphQl, app) {
 		try {
 			const mutation = await authenticatedGraphQl(ADD_COMMENT, {
 				id: req.body.bountyId,
-				body: `A deposit of  ${formattedVolume} ${name} was placed on this issue at ${process.env.BASE_URL}/bounty/${req.body.bountyId}/${req.body.id}`,
+				body: `A deposit of  ${formattedVolume} ${name} was placed on this issue at ${process.env.BASE_URL}/contract/${req.body.bountyId}/${req.body.id}`,
 			});
 			res.json(mutation);
 		} catch (err) {
